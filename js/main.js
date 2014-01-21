17fc37d3f3d879edd80a3aada7ae46d42bcc1171
davidkclee <davidkclee1984@gmail.com>
2014-01-21 12:53:07 +0800

    test

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+
+$("#btnCars").bind('touchstart mousedown', function(){
+	var term= "button=" + $(this).val();
+	$.ajax({
+		url:'reply.php',
+		type:'POST',
+		data:term,
+		dataType:'json',
+		error:function(jqXHR,text_status,strError){
+			alert("no connection");
+		},
+		timeout:60000,
+		success:function(data){
+			$("#result").html("");
+			for(var i in data){
+				$("#result").append("<li>"+data[i]+"</li>");
+			}
+		}
+	});
+});
+
+$("#btnBikes").bind('touchstart mousedown', function(){
+	var term= "button=" + $(this).val();
+	$.ajax({
+		url:'reply.php',
+		type:'POST',
+		data:term,
+		dataType:'json',
+		error:function(jqXHR,text_status,strError){
+			alert("no connection");
+		},
+		timeout:60000,
+		success:function(data){
+			$("#result").html("");
+			for(var i in data){
+				$("#result").append("<li>"+data[i]+"</li>");
+			}
+		}
+	});
+});
+
+});
