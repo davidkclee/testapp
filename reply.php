524a0e73d742023f3d2d9a676642ec79d962866e
davidkclee <davidkclee1984@gmail.com>
2014-01-21 16:59:35 +0800

    12

--- reply.php
+++ reply.php
@@ -1,3 +1,23 @@
 <?php
-    echo "Hello " . $_POST["name"] . "Nice to meet you";
+	$name = $_POST["name"];
+	if($name == "david")
+	{
+		$data = array(
+            "Name"     => "David",
+            "Surname"  => "Lee",
+            "Gender"   => "Male",
+            "Email"      => "davidkclee1984@gmail.com"
+        );
+        echo json_encode($data);
+	}
+	else
+	{
+		$data = array(
+            "Name"     => "Unknown",
+            "Surname"  => "Unknown",
+            "Gender"   => "Unknown",
+            "Email"      => "Unknown"
+        );
+        echo json_encode($data);
+	}
 ?>
