349b67d797b492e09622a2391cb7fcfda0a90ea1
davidkclee <davidkclee1984@gmail.com>
2014-01-21 15:06:45 +0800

    4

--- reply.php
+++ reply.php
@@ -1,4 +1,7 @@
 <?php
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
 $choice =$_POST["button"];
 $cars = array("Honde", "BMW", "Ferrari");
 $bikes = array("Ducaite", "Royal Enfield", "Harley Davidson");
