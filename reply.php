0cfbbf446b04992967a7ccc3c1b5609bff0422c6
davidkclee <davidkclee1984@gmail.com>
2014-01-21 16:46:03 +0800

    11

--- reply.php
+++ reply.php
@@ -1,17 +1,3 @@
 <?php
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Cache-Control: no-cache");
-header("Pragma: no-cache");
-$choice =$_POST["button"];
-$cars = array("Honde", "BMW", "Ferrari");
-$bikes = array("Ducaite", "Royal Enfield", "Harley Davidson");
-if($choice == "cars") 
-{
-	print json_encode($cars);
-}
-else 
-{
-	print json_encode($bikes);
-}
-
+    echo "Hello " . $_POST["name"] . "Nice to meet you";
 ?>
